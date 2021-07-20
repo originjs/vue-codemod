2d81114185ec30dfa32a4e2b6b46f5e3e60a7e02
ruleeeer <me@ruleeeer.cn>
2021-07-20 16:44:04 +0800

    feat: implements element-ui format attribute in the picker-option move to top-level

--- vue-transformations/element-ui/time-picker-format-attribute.ts
+++ vue-transformations/element-ui/time-picker-format-attribute.ts
@@ -1,5 +1,5 @@
 import { Node, VAttribute } from 'vue-eslint-parser/ast/nodes'
-import  * as OperationUtil from '../../src/operationUtils'
+import * as OperationUtil from '../../src/operationUtils'
 import type { Operation } from '../../src/operationUtils'
 import {
   default as wrap,
@@ -14,28 +14,37 @@ export const transformAST = createTransformAST(
 export default wrap(transformAST)
 
 function nodeFilter(node: Node): boolean {
-  // @ts-ignore
-  const pre = (item: VAttribute) => item.key.name === 'bind' && item.key.argument?.name === 'format'
-  return node.type === 'VAttribute'
-    && node.key.type === 'VDirectiveKey'
-    && node.key.name.name === 'bind'
+  const pre = (item: VAttribute) =>
     // @ts-ignore
-    && node.key.argument.name === 'picker-options'
-    && node.value?.type === 'VExpressionContainer'
-    && node.value.expression?.type === 'ObjectExpression'
+    item.key.name === 'bind' && item.key.argument?.name === 'format'
+  return (
+    node.type === 'VAttribute' &&
+    node.key.type === 'VDirectiveKey' &&
+    node.key.name.name === 'bind' &&
     // @ts-ignore
-    && node.value.expression.properties.filter(item => item.key.name === 'format').length > 0
-    && node.parent.parent.type === 'VElement'
-    && node.parent.parent.name === 'el-time-picker'
-    && node.parent.attributes.filter(pre).length === 0
+    node.key.argument.name === 'picker-options' &&
+    node.value?.type === 'VExpressionContainer' &&
+    node.value.expression?.type === 'ObjectExpression' &&
+    // @ts-ignore
+    node.value.expression.properties.filter(item => item.key.name === 'format')
+      .length > 0 &&
+    node.parent.parent.type === 'VElement' &&
+    node.parent.parent.name === 'el-time-picker' &&
+    node.parent.attributes.filter(pre).length === 0
+  )
 }
 
 function fix(node: VAttribute): Operation[] {
   let fixOperations: Operation[] = []
-//  get format attribute in the time-picker
+  //  get format attribute in the time-picker
   // @ts-ignore
-  let formatValue = node.value.expression.properties.filter(item => item.key.name === 'format')[0].value.value
-//  add format attribute to el-time-picker tag
-  fixOperations.push(OperationUtil.insertTextBefore(node, `format='${formatValue}' `))
-  return fixOperations;
+  let formatValue = node.value.expression.properties.filter(
+    // @ts-ignore
+    item => item.key.name === 'format'
+  )[0].value.value
+  //  add format attribute to el-time-picker tag
+  fixOperations.push(
+    OperationUtil.insertTextBefore(node, `format='${formatValue}' `)
+  )
+  return fixOperations
 }
