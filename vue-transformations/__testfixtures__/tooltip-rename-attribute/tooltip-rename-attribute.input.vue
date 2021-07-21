22c1577c715e29a2f070d83ad9060cc6a2c7880c
ruleeeer <me@ruleeeer.cn>
2021-07-21 16:24:04 +0800

    feat: implements element-ui open-delay and show-after rename to show-after and auto-close in the el-tooltip

---
+++ vue-transformations/__testfixtures__/tooltip-rename-attribute/tooltip-rename-attribute.input.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-tooltip
+    effect='dark'
+    content='Bottom Right 提示文字'
+    placement='bottom-end'
+    open-delay='1000'
+    hide-after='3000'>
+    <el-button>下右</el-button>
+  </el-tooltip>
+
+  <div
+    open-delay='1000'
+    hide-after='3000'>
+  </div>
+</template>
